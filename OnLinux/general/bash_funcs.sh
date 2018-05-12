c008c63a31e4013469e99893f707c134414870d7
Sun Rongyang <sun-rongyang@outlook.com>
2018-05-13 06:17:08 +0800

    Update looptest()

--- OnLinux/general/bash_funcs.sh
+++ OnLinux/general/bash_funcs.sh
@@ -1,4 +1,19 @@
+#
+# Author: Rongyang Sun <sun-rongyang@outlook.com>
+# Creation Date: 2018-05-13 06:03
+# 
+# Description: Useful bash functions collection.
+#
+
+
 # run something until it fails.
 looptest() {
-  i=0; while [ $i -eq 0 ]; do "$@"; i=$?; done
+  EXIT_CODE=0
+  COUNT=0
+  while [[ ${EXIT_CODE} -eq 0 ]]; do
+    "$@"
+    EXIT_CODE=$?
+    COUNT=`expr ${COUNT} + 1`
+    echo "execution times: " ${COUNT}
+  done
 }
